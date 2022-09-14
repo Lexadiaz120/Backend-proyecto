42fb01442d0066eef5283bfc68d0856d76fc5514
unknown <daura38@gmail.com>
2022-09-14 22:12:06 +0200

    update files

--- scraper.js
+++ scraper.js
@@ -1,5 +1,7 @@
 const { link } = require("fs");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
 function run() {
   return new Promise(async (resolve, reject) => {
     try {
@@ -8,8 +10,12 @@ function run() {
         defaultViewport: null,
         args: ["--incognito"],
       });
+      let pagesToScrape = 2;
+      let currentPage = 1;
       const page = await browser.newPage();
-      await page.goto("https://openwebinars.net/buscador/?s=frontend");
+      await page.goto("https://openwebinars.net/buscador/?s=frontend", {
+        waitUntil: "networkidle2",
+      });
       let list = [];
       await page.waitForSelector(".list-unstyled");
       const cursos = await page.$(".list-unstyled");
@@ -42,10 +48,35 @@ function run() {
       });
       list = list.concat(cursitos);
       console.log(list);
-      for (linktogo of resultsvalues) {
-        await page.waitForSelector(".list-unstyled");
-        let base = "https://openwebinars.net";
-        await page.goto(base + linktogo.url);
+      while (currentPage <= pagesToScrape) {
+        if (currentPage < pagesToScrape) {
+          for (linktogo of resultsvalues) {
+            let base = "https://openwebinars.net";
+            await page.goto(
+              "https://openwebinars.net/buscador/?s=frontend&page=2",
+              {
+                waitUntil: "networkidle2",
+              }
+            );
+            await page.waitForSelector(".list-unstyledffwe");
+            let cursitos = await page.$(".list-unstyledffwe");
+            console.log(cursitos, "cursitos");
+            let cursos = await cursitos.evaluate((node) => {
+              // for (let curso of cursos) {
+              //   list.push({
+              //     description: curso.querySelector("p").textContent,
+              //     title: curso.querySelector(".card-title").textContent,
+              //   });
+              //   return list;
+              // }
+              let cursos = node.querySelectorAll(".mb-3");
+              console.log(cursos);
+            });
+            list = list.concat(cursos);
+            console.log(cursos);
+          }
+        }
+        currentPage++;
       }
     } catch (e) {
       return reject(e);
